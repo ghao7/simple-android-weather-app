27c7b9bdfbdec61e0b02554ab35bd3e6326f62b8
guhao6869 <guhao6869@gmail.com>
2017-06-14 12:45:10 +0800

    add copy file util

--- app/src/main/java/com/example/guhao/myweather/data/DBOperation.java
+++ app/src/main/java/com/example/guhao/myweather/data/DBOperation.java
@@ -3,6 +3,7 @@ package com.example.guhao.myweather.data;
 import android.content.Context;
 
 import com.example.guhao.myweather.data.database.MyDatabaseHelper;
+import com.example.guhao.myweather.util.CopyFileUtil;
 
 /**
  * Created by guhao on 6/14/17.
@@ -14,6 +15,12 @@ public class DBOperation {
     public DBOperation(Context context){
         dbHelper = new MyDatabaseHelper(context, "china-city-list.db", null, 1);
         dbHelper.getWritableDatabase();
+
+        copyDbFile();
     }
 
+    public void copyDbFile(){
+        CopyFileUtil.copyFile("/data/data/com.example.guhao.myweather/databases/china-city-list.db",
+                "/res/test.db");
+    }
 }
---
+++ app/src/main/java/com/example/guhao/myweather/util/CopyFileUtil.java
@@ -0,0 +1,32 @@
+package com.example.guhao.myweather.util;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Created by guhao on 6/14/17.
+ */
+
+public class CopyFileUtil{
+    public static void copyFile(String source, String dest){
+        InputStream input = null;
+        OutputStream output = null;
+        try {
+            input = new FileInputStream(source);
+            output = new FileOutputStream(dest);
+            byte[] buf = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = input.read(buf)) > 0) {
+                output.write(buf, 0, bytesRead);
+            }
+
+            input.close();
+            output.close();
+        }catch (IOException e) {
+            System.out.print(e.getMessage());
+        }
+    }
+}
