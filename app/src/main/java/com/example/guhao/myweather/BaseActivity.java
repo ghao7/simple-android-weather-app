09785ebbe9ec4e5bdf1e9cff1769de10eb537022
guhao6869 <guhao6869@gmail.com>
2017-06-22 17:50:18 +0800

    微调界面 添加toolbar动画效果

--- app/src/main/java/com/example/guhao/myweather/BaseActivity.java
+++ app/src/main/java/com/example/guhao/myweather/BaseActivity.java
@@ -6,6 +6,7 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
 /**
@@ -15,7 +16,7 @@ import android.widget.Toast;
  * Desc:
  */
 
-public abstract class BaseActivity extends Activity {
+public abstract class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/guhao/myweather/CityListScrollingActivity.java
+++ app/src/main/java/com/example/guhao/myweather/CityListScrollingActivity.java
@@ -1,13 +1,23 @@
 package com.example.guhao.myweather;
 
+import android.app.SearchManager;
+import android.app.Service;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.app.Activity;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -17,19 +27,43 @@ import com.example.guhao.myweather.adapter.CityRecycleViewAdapter;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CityListScrollingActivity extends Activity {
+public class CityListScrollingActivity extends BaseActivity {
+    private final String TAG = "";
     private FloatingActionButton fab;
     private RecyclerView recyclerView;
+    private Toolbar tb_toolbar;
+
     List<String> mlist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_city_list_scrolling);
-
         findView();
         initData();
         fabListener();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+        Log.d(TAG, "onCreateOptionsMenu: ");
+
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Log.d(TAG, "onOptionsItemSelected: ");
+        switch (item.getItemId()){
+            case R.id.action_search:
+                showShort("Clicked");
+                break;
+
+            default:
+        }
+        return true;
     }
 
     public void initData(){
@@ -39,11 +73,14 @@ public class CityListScrollingActivity extends Activity {
         recyclerView.setAdapter(new CityRecycleViewAdapter(mlist));
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         //recyclerView.addItemDecoration();
+
+        setSupportActionBar(tb_toolbar);
+
     }
 
     public void initList(){
         mlist = new ArrayList<>();
-        for (int i = 0; i < 30; i++){
+        for (int i = 0; i < 100; i++){
             mlist.add(i+"");
         }
     }
@@ -51,7 +88,7 @@ public class CityListScrollingActivity extends Activity {
     public void findView(){
         fab = (FloatingActionButton)findViewById(R.id.fab);
         recyclerView = (RecyclerView)findViewById(R.id.city_list_recycler_view);
-
+        tb_toolbar = (Toolbar)findViewById(R.id.tb_toolbar);
     }
 
     public void fabListener(){
